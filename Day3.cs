bdd0a6e0260f2dbdb79927aafae395e947b93817
Arthur Rosa <arthur@rosafamily.net>
2025-12-02 21:45:39 -0800

    Day 3
    ╭───────┬────────┬─────────────────┬──────────────╮
    │ Day   │ Part   │ Solution        │ Elapsed time │
    ├───────┼────────┼─────────────────┼──────────────┤
    │ Day 3 │ Part 1 │ 17087           │ 5 ms         │
    │ Day 3 │ Part 2 │ 169019504359949 │ 0.887 ms     │
    │       │        │                 │              │
    ╰───────┴────────┴─────────────────┴──────────────╯

---
+++ Day3.cs
@@ -0,0 +1,56 @@
+using AdventOfCode2025.Shared;
+using JetBrains.Annotations;
+
+namespace AdventOfCode2025;
+
+[UsedImplicitly]
+public class Day3 : Day
+{
+    public Day3()
+    {
+        Test = new TestCase("357", "3121910778619");
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var result = GetInputLines().AsParallel().Select(line =>
+        {
+            var batteries = line.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray();
+            var highestIdx = FindHighestIndex(batteries, 0, 1);
+            var nextHighestIdx = FindHighestIndex(batteries, highestIdx + 1, 0);
+            return long.Parse($"{batteries[highestIdx]}{batteries[nextHighestIdx]}");
+        }).Sum().ToString();
+        return new ValueTask<string>(result);
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var result = GetInputLines().AsParallel().Select(line =>
+        {
+            var batteries = line.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray();
+            var result = "";
+            var highestIdx = -1;
+            var cutoff = 11;
+            do // sue me, I don't care
+            {
+                highestIdx = FindHighestIndex(batteries, highestIdx + 1, cutoff);
+                result += batteries[highestIdx].ToString();
+                cutoff--;
+            } while (cutoff >= 0);
+
+            return long.Parse(result);
+        }).Sum().ToString();
+        return new ValueTask<string>(result);
+    }
+
+    private static int FindHighestIndex(int[] batteries, int startIndex, int cutoff)
+    {
+        var highestIdx = startIndex;
+        for (var i = highestIdx; i < batteries.Length - cutoff; i++)
+        {
+            if (batteries[i] > batteries[highestIdx]) highestIdx = i;
+        }
+
+        return highestIdx;
+    }
+}
--- Shared/Day.cs
+++ Shared/Day.cs
@@ -9,7 +9,6 @@ public abstract class Day : BaseDay
     protected string Input { get; private set; }
     public record TestCase(string? ExpectedPart1, string? ExpectedPart2);
     public TestCase? Test { get; set; }
-    protected string SampleInput { get; } = "";
 
     protected Day()
     {
@@ -18,12 +17,6 @@ public abstract class Day : BaseDay
         AsyncHelper.RunSync(async () => await Utils.GetInputData(Year, day, InputFilePath));
         // ReSharper disable once VirtualMemberCallInConstructor
         Input = Utils.ReadInput(InputFilePath).Trim();
-
-        var sampleFilePath = Path.Combine("Inputs", $"{day:D2}Sample.txt");
-        if (File.Exists(sampleFilePath))
-        {
-            SampleInput = Utils.ReadInput(sampleFilePath).Trim();
-        }
     }
 
     protected IEnumerable<string> GetInputLines()
@@ -33,43 +26,55 @@ public abstract class Day : BaseDay
 
     public async Task RunTests()
     {
-        var originalInput = Input;
-        Console.WriteLine($"Running tests for Day {CalculateIndex()}...");
-        if (Test?.ExpectedPart1 != null)
+        var sampleFilePath = Path.Combine("Inputs", $"{(int)CalculateIndex():D2}Sample.txt");
+        if (File.Exists(sampleFilePath))
         {
-            Input = SampleInput.Trim();
-            var result1 = await Solve_1();
-            if (result1 != Test.ExpectedPart1)
+            var sampleInput = Utils.ReadInput(sampleFilePath).Trim();
+            var originalInput = Input;
+            Console.WriteLine($"Running tests for Day {CalculateIndex()}...");
+            if (Test?.ExpectedPart1 != null)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"[TEST FAILED] Part 1. Expected: {Test.ExpectedPart1}, Got: {result1}");
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"[TEST PASSED] Part 1.");
+                Input = sampleInput.Trim();
+                var result1 = await Solve_1();
+                if (result1 != Test.ExpectedPart1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"[TEST FAILED] Part 1. Expected: {Test.ExpectedPart1}, Got: {result1}");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("[TEST PASSED] Part 1.");
+                }
+
+                Console.ResetColor();
             }
 
+            if (Test?.ExpectedPart2 != null)
+            {
+                Input = sampleInput.Trim();
+                var result2 = await Solve_2();
+                if (result2 != Test.ExpectedPart2)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"[TEST FAILED] Part 2. Expected: {Test.ExpectedPart2}, Got: {result2}");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("[TEST PASSED] Part 2.");
+                }
+
+                Console.ResetColor();
+            }
+
+            Input = originalInput;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[TEST FAILED] Sample input was not found.");
             Console.ResetColor();
         }
-
-        if (Test?.ExpectedPart2 != null)
-        {
-            Input = SampleInput.Trim();
-            var result2 = await Solve_2();
-            if (result2 != Test.ExpectedPart2)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"[TEST FAILED] Part 2. Expected: {Test.ExpectedPart2}, Got: {result2}");
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"[TEST PASSED] Part 2.");
-            }
-
-            Console.ResetColor();
-        }
-        Input = originalInput;
     }
 }
