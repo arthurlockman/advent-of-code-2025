1486e98346da677ab0d22acdd7bd82fb34a07e9d
Arthur Rosa <arthur@rosafamily.net>
2025-12-09 09:39:44 -0800

    Day 9
    ╭───────┬────────┬────────────┬──────────────╮
    │ Day   │ Part   │ Solution   │ Elapsed time │
    ├───────┼────────┼────────────┼──────────────┤
    │ Day 9 │ Part 1 │ 4715966250 │ 110 ms       │
    │ Day 9 │ Part 2 │ 1530527040 │ 4.641 s      │
    │       │        │            │              │
    ╰───────┴────────┴────────────┴──────────────╯

--- Day9.cs
+++ Day9.cs
@@ -1,4 +1,5 @@
 using AdventOfCode2025.Shared;
+using NetTopologySuite.Geometries;
 
 namespace AdventOfCode2025;
 
@@ -20,62 +21,6 @@ internal class Rectangle2D
                                                  (Corner1 == other.Corner2 && Corner2 == other.Corner1));
 
     public override int GetHashCode() => HashCode.Combine(Corner1, Corner2);
-
-    public Point2D[] GetCorners() => [Corner1, Corner2, new(Corner1.X, Corner2.Y), new(Corner2.X, Corner1.Y)];
-}
-
-internal class Polygon2D
-{
-    public Point2D[] Corners { get; }
-
-    private readonly List<(Point2D, Point2D)> _lineSegments = [];
-
-    public Polygon2D(Point2D[] corners)
-    {
-        Corners = corners;
-        for (var i = 0; i < corners.Length - 1; i++)
-        {
-            _lineSegments.Add(new ValueTuple<Point2D, Point2D>(corners[i], corners[i + 1]));
-        }
-        _lineSegments.Add(new ValueTuple<Point2D, Point2D>(corners[^1], corners[0]));
-    }
-
-    public bool ContainsPoint(Point2D point)
-    {
-        // the basic premise here is the conjecture that if a ray cast from a point in a fixed direction
-        // intersects the polygon an odd number of times, the point is within the polygon. if it intersects
-        // an even number of times, it's outside the polygon.
-        // what we do then is turn this arbitrary polygon into a collection of line segments, then cast a ray
-        // from our point in a fixed direction (in this case toward 0,0) and see how many line segments it intersects.
-        // https://en.wikipedia.org/wiki/Point_in_polygon#Ray_casting_algorithm
-        // https://www.reddit.com/r/algorithms/comments/9moad4/what_is_the_simplest_to_implement_line_segment/
-        long Cross(Point2D a, Point2D b)
-        {
-            return a.X * b.Y - a.Y * b.X;
-        }
-
-        long Orient(Point2D a, Point2D b, Point2D c)
-        {
-            return Cross(new Point2D(b.X - a.X, b.Y - a.Y), new Point2D(c.X - a.X, c.Y - a.Y));
-        }
-
-        bool Intersects(Point2D a, Point2D b, Point2D c, Point2D d)
-        {
-            long oa = Orient(c, d, a),
-                ob = Orient(c, d, b),
-                oc = Orient(a, b, c),
-                od = Orient(a, b, d);
-            // Proper intersection exists iff opposite signs
-            return (oa * ob < 0 && oc * od < 0);
-        }
-
-        // if the point is one of the corners of the polygon it's obviously in the polygon
-        if (Corners.Any(p => p.Equals(point))) return true;
-
-        // if it's not then we need to check if it's contained using the raycasting method
-        var intersections = _lineSegments.Count(l => Intersects(l.Item1, l.Item2, point, new Point2D(0, 0)));
-        return intersections % 2 != 0;
-    }
 }
 
 public class Day9 : Day
@@ -98,15 +43,24 @@ public class Day9 : Day
 
     public override ValueTask<string> Solve_2()
     {
-        var corners = InputLines.Value.Select(l => new Point2D(l)).ToList();
-        var boundingPolygon = new Polygon2D(corners.ToArray());
-        var rectangles = corners.SelectMany(c1 => corners.Select(c2 => new Rectangle2D(c1, c2)))
+        var coordinates = InputLines.Value.Select(l =>
+        {
+            var split = l.Split(',');
+            return new Coordinate(int.Parse(split[0]), int.Parse(split[1]));
+        }).ToList();
+        coordinates.Add(coordinates[0]);
+
+        var geoFactory = new GeometryFactory();
+        var boundingPolygon = geoFactory.CreatePolygon(coordinates.ToArray());
+
+        var rectangles = coordinates.SelectMany(c1 => coordinates.Select(c2 => (
+                geoFactory.ToGeometry(new Envelope(c1, c2)),
+                (Math.Abs(c2.X - c1.X) + 1) * (Math.Abs(c2.Y - c1.Y) + 1))))
             .Distinct()
             .AsParallel()
-            .Where(r => !Equals(r.Corner1, r.Corner2))
-            .Where(r => r.GetCorners().All(c => boundingPolygon.ContainsPoint(c)))
-            .OrderByDescending(c => c.Area)
+            .Where(r => r.Item1.Within(boundingPolygon))
+            .OrderByDescending(c => c.Item2)
             .ToArray();
-        return new ValueTask<string>($"{rectangles[0].Area}");
+        return new ValueTask<string>($"{rectangles[0].Item2}");
     }
 }
